7bf91d0e74182d5fc13920fb3cbe59b458c14d7c
kaekae1015 <52931934+kaekae1015@users.noreply.github.com>
2019-07-15 16:01:15 -0400

    Add files via upload

---
+++ Coding101.Day1 KH.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("hello world")
+
+5/8     #division
+
+print("I am learning to code in python today")
+
+5*2
+
+5/2
+
+5-2 
+
+5^2
+
+(8/9)*3
+
+print("learning how to code is awesome")
+
+print("the division of 5/3 is", 5/2)
+
+#create function to sum any two numbers
+
+def plus(a,b):
+    plus = a + b
+    print(plus)
+    
+plus(2.8,3)
+
+def plus(a,b,c):
+    plus = a + b + c
+    print(plus)
+    
+plus(15,476,32)
+
+#create function of an area of a triangle
+
+def multiply(a,b,c):
+    multiply = a * b * c
+    print(multiply)
+    
+multiply(1/2,3.75,5.25)
