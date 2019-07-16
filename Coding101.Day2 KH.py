b20c91cd3f523bf8bb2289d95300352df0f77112
kaekae1015 <52931934+kaekae1015@users.noreply.github.com>
2019-07-16 16:02:17 -0400

    Add files via upload

---
+++ Coding101.Day2 KH.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 16 10:26:11 2019
+
+@author: admin
+"""
+
+(5/3 * 4)
+
+("Hola")
+
+#Chocolate types
+d = "dark"
+m = "milk"
+w = "white"
+
+print(d/m/w)
+print(d,m,w)
+
+#creating a function to greet a person by their name
+
+print("Greetings Shakayla")
+print("Good afternoon Kae'Driona")
+
+m = "Miss Amita"
+print("How are you doing today",m,"?")
+
+def hello(name):
+    print(name)
+    
+hello("Kim")
+
+def printer(a):
+    a = (name)
+    print("Hi",a)
+    
+    
+printer("Mary")
+
+import math
+
+dir(math)
+
+math.p
+
+math.sqrt(10)
+math.pow(2,4)
+math.factorial(8)
+math.pow(6,8)
+math.remainder(5,3)
+
+def  cube(a):
+     cube = a / a / a
+     print(cube)
+     
+ cube(666)    
+     
+ math.pow(5,3)    
+math.pow(125,1/3)   
+
+math.pow(a,b)
+math.pow(a,1/3)
+
+def cube(y):
+    cube = math.pow(y,1/3)
+    print("the cube root of",y,"is",cube)
+    
+cube(8)
+     
+#Writing more functions
+
+q = "Please enter a value to the variable","t"
+print(q,cube(27))
+     
+     
